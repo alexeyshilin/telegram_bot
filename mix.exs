0da1330e028dc9477e022c2f8b9dfc67ea9ed30a
Alexey Shilin <shilin.alexey@gmail.com>
2021-11-25 01:02:34 +0300

    Add: telegram dependency

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule Telegrambot.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
 
-      {:timex, "~> 3.0"}
+      {:timex, "~> 3.0"},
+      {:telegram, git: "https://github.com/visciang/telegram.git", tag: "0.5.0"}
+
     ]
   end
 end
