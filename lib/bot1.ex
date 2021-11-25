f38676d763a3f88bb0574a6ba6b415f2e4b1676e
Alexey Shilin <shilin.alexey@gmail.com>
2021-11-25 20:54:32 +0300

    Fix: simple echo bot

--- lib/bot1.ex
+++ lib/bot1.ex
@@ -27,6 +27,26 @@ defmodule Bot1 do
 
   end
 
+  def handle_update(
+        token,
+        data_raw = %{}
+      ) do
+
+      # data_raw
+      # %{"channel_post" => %{"chat" => %{"id" => -1001728410621, "title" => "test", "type" => "channel", "username" => "ffatherfrost"}, "date" => 1637862242, "message_id" => 20, "sender_chat" => %{"id" => -1001728410621, "title" => "test", "type" => "channel", "username" => "ffatherfrost"}, "text" => "111"}, "update_id" => 521426571}
+
+      message_id = data_raw["channel_post"]["message_id"]
+      chat_id = data_raw["channel_post"]["chat"]["id"]
+      text = data_raw["channel_post"]["text"]
+
+    Telegram.Api.request(token, "sendMessage",
+      chat_id: chat_id,
+#      reply_to_message_id: message_id,
+      text: "Hey! You sent me a some message: #{text}"
+    )
+
+  end
+
 end
 
 # {:ok, _} = Bot1.start_link()
