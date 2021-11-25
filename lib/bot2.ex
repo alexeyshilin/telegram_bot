562bb9275c14edec667996a6831e6fa99c30b80e
Alexey Shilin <shilin.alexey@gmail.com>
2021-11-25 21:17:27 +0300

    Fix: fibonacci bot

--- lib/bot2.ex
+++ lib/bot2.ex
@@ -18,6 +18,39 @@ defmodule Bot2 do
          text: "Fib[#{intVal}] = #{Fib.fib(intVal)}")
     end
 
+
+  def handle_update(
+        token,
+        data_raw = %{}
+      ) do
+
+      # data_raw
+      # %{"channel_post" => %{"chat" => %{"id" => -1001728410621, "title" => "test", "type" => "channel", "username" => "ffatherfrost"}, "date" => 1637862242, "message_id" => 20, "sender_chat" => %{"id" => -1001728410621, "title" => "test", "type" => "channel", "username" => "ffatherfrost"}, "text" => "111"}, "update_id" => 521426571}
+
+      message_id = data_raw["channel_post"]["message_id"]
+      chat_id = data_raw["channel_post"]["chat"]["id"]
+      text = data_raw["channel_post"]["text"]
+
+      "/fib"<>val = text
+
+      intval = val |> parse_seconds_arg()
+
+    Telegram.Api.request(token, "sendMessage",
+      chat_id: chat_id,
+      reply_to_message_id: message_id,
+      text: "Fib[#{intval}] = #{Fib.fib(intval)}"
+    )
+
+  end
+
+  defp parse_seconds_arg(arg) do
+    default_arg = "1"
+    arg = if arg == "", do: default_arg, else: arg
+    {seconds, ""} = arg |> String.trim() |> Integer.parse()
+    seconds
+  end
+
+
     any do
        IO.puts "not found"
     end
